6659ebf46e1dc62578564b797feec2b1529dda23
Sebastien Chauvin <sebastien.chauvin@gmail.com>
2017-01-03 14:36:25 +0100

    Reformatting, method spelling corrected

--- library/src/main/java/com/test/tudou/library/weekpager/view/WeekDayViewPager.java
+++ library/src/main/java/com/test/tudou/library/weekpager/view/WeekDayViewPager.java
@@ -6,104 +6,110 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
+
 import com.test.tudou.library.model.CalendarDay;
-import com.test.tudou.library.util.DayUtils;
 
 /**
  * Created by tudou on 15-5-1.
  */
 public class WeekDayViewPager extends ViewPager implements WeekView.OnDayClickListener {
 
-  private final static int PAGE_SCROLL_DELAY = 24;
+    private final static int PAGE_SCROLL_DELAY = 24;
 
-  public interface DayScrollListener {
-    public void onDayPageScrolled(int position, float positionOffset,
-        int positionOffsetPixels);
-    public void onDayPageSelected(int position);
-    public void onDayPageScrollStateChanged(int state);
-  }
+    public interface DayScrollListener {
+        public void onDayPageScrolled(int position, float positionOffset,
+                                      int positionOffsetPixels);
 
-  private RecyclerView mRecyclerView;
-  private DayScrollListener mDayScrollListener;
+        public void onDayPageSelected(int position);
 
-  public WeekDayViewPager(Context context) {
-    this(context, null);
-  }
+        public void onDayPageScrollStateChanged(int state);
+    }
 
-  public WeekDayViewPager(Context context, AttributeSet attrs) {
-    super(context, attrs);
-  }
+    private RecyclerView mRecyclerView;
+    private DayScrollListener mDayScrollListener;
 
-  @Override public void onDayClick(WeekView simpleMonthView, CalendarDay calendarDay, int position) {
-    setCurrentItem(position);
-  }
+    public WeekDayViewPager(Context context) {
+        this(context, null);
+    }
 
-  public void setWeekRecyclerView(RecyclerView recyclerView) {
-    mRecyclerView = recyclerView;
-    updateListener();
-  }
+    public WeekDayViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-  public void setDayScrollListener(DayScrollListener dayScrollListener) {
-    mDayScrollListener = dayScrollListener;
-  }
+    @Override
+    public void onDayClick(WeekView simpleMonthView, CalendarDay calendarDay, int position) {
+        setCurrentItem(position);
+    }
 
-  private void updateListener() {
-    setOnPageChangeListener(new OnPageChangeListener() {
-      @Override public void onPageScrolled(int position, float positionOffset,
-          int positionOffsetPixels) {
-        onDayPaegScrolled(position, positionOffset, positionOffsetPixels);
-        int i = 0;
-        View child = mRecyclerView.getChildAt(i);
+    public void setWeekRecyclerView(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
+        updateListener();
+    }
 
-        if (child == null) return;
-        while (child != null && child.getRight() <= 0) {
-          child = mRecyclerView.getChildAt(++i);
+    public void setDayScrollListener(DayScrollListener dayScrollListener) {
+        mDayScrollListener = dayScrollListener;
+    }
+
+    private void updateListener() {
+        setOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+                onDayPageScrolled(position, positionOffset, positionOffsetPixels);
+                int i = 0;
+                View child = mRecyclerView.getChildAt(i);
+
+                if (child == null) return;
+                while (child != null && child.getRight() <= 0) {
+                    child = mRecyclerView.getChildAt(++i);
+                }
+                ((WeekView) child).onViewPageScroll(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                onDayPageSelected(position);
+                mRecyclerView.smoothScrollToPosition(position / 7);
+                for (int j = 0; j < mRecyclerView.getChildCount(); j++) {
+                    View week = mRecyclerView.getChildAt(j);
+                    if (week instanceof WeekView) {
+                        week.invalidate();
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                onDayPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    private void onDayPageScrolled(int position, float positionOffset,
+                                   int positionOffsetPixels) {
+        if (mDayScrollListener != null) {
+            mDayScrollListener.onDayPageScrolled(position, positionOffset, positionOffsetPixels);
         }
-        ((WeekView) child).onViewPageScroll(position, positionOffset, positionOffsetPixels);
-      }
+    }
 
-      @Override public void onPageSelected(int position) {
-        onDayPageSelected(position);
-        mRecyclerView.smoothScrollToPosition(position / 7);
-        for (int j = 0; j < mRecyclerView.getChildCount(); j++) {
-          View week = mRecyclerView.getChildAt(j);
-          if (week instanceof WeekView) {
-            week.invalidate();
-          }
+    private void onDayPageSelected(int position) {
+        if (mDayScrollListener != null) {
+            mDayScrollListener.onDayPageSelected(position);
         }
-      }
-
-      @Override public void onPageScrollStateChanged(int state) {
-        onDayPageScrollStateChanged(state);
-      }
-    });
-  }
-
-  private void onDayPaegScrolled(int position, float positionOffset,
-      int positionOffsetPixels) {
-    if (mDayScrollListener != null) {
-      mDayScrollListener.onDayPageScrolled(position, positionOffset, positionOffsetPixels);
     }
-  }
 
-  private void onDayPageSelected(int position) {
-    if (mDayScrollListener != null) {
-      mDayScrollListener.onDayPageSelected(position);
+    private void onDayPageScrollStateChanged(int state) {
+        if (mDayScrollListener != null) {
+            mDayScrollListener.onDayPageScrollStateChanged(state);
+        }
     }
-  }
 
-  private void onDayPageScrollStateChanged(int state) {
-    if (mDayScrollListener != null) {
-      mDayScrollListener.onDayPageScrollStateChanged(state);
+    public void setCurrentPosition(final int item) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setCurrentItem(item);
+            }
+        }, PAGE_SCROLL_DELAY);
     }
-  }
-
-  public void setCurrentPosition(final int item) {
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-        setCurrentItem(item);
-      }
-    }, PAGE_SCROLL_DELAY);
-  }
 }
